73a6f39241dcd40354e1a93375e2900d4a7b9553
Serega219 <sergeyfns15@gmail.com>
2023-05-24 22:06:55 +0300

    new 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1000;
-        int b = 5000;
+        int startamount = 1000; // начальная сумма на счету
+        int debitamount = 5000; // сумма пополнения
 
-        if (b > 1000) {
-            System.out.println((b-1000) * 1.0 / 100);
+        int points; // количество бонусных рублей
+        if (debitamount > 1000) { // проверяем, что сумма пополнения больше тысячи
+            points = (debitamount / 100) * 1; // вычисляем бонус как один процент от количества полных ста рублей
         } else {
-            System.out.println("сумма пополнения меньше тысячи");
+            points = 0; // иначе бонус равен нулю
+            System.out.println("сумма пополнения меньше тысячи"); // выводим сообщение
         }
+        startamount += debitamount + points; // прибавляем к начальной сумме сумму пополнения и бонус
+        System.out.println("итоговая сумма на счету: " + startamount); // выводим итоговую сумму
+        System.out.println("количество бонусных рублей: " + points); // выводим количество бонусных рублей
     }
 }
